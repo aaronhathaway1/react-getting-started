42e19a0c944ab59262d56733c353d3e91cbd4604
Aaron Hathaway <aaronhathaway1@hotmail.com>
2023-05-27 16:27:47 -0600

    Finished

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -36,7 +36,7 @@ const ExpenseForm = (props) => {
 
 		const expenseData = {
 			title: userInput.enteredTitle,
-			amount: userInput.enteredAmount,
+			amount: +userInput.enteredAmount,
 			date: new Date(userInput.enteredDate),
 		};
 
@@ -82,6 +82,9 @@ const ExpenseForm = (props) => {
 				</div>
 			</div>
 			<div className='new-expense__actions'>
+				<button type='button' onClick={props.onCancel}>
+					Cancel
+				</button>
 				<button type='submit'>Add Expense</button>
 			</div>
 		</form>
